11bd4abc6bc14a6950b8048eccd34db205f409f1
Qiu-Weidong <568385476@qq.com>
2022-12-06 21:37:43 +0800

    .

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -3,7 +3,7 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+// import MenuIcon from '@mui/icons-material/Menu';
 import Search from '../components/Search';
 import { Button } from '@mui/material';
 import logo from '../assets/icon.svg';
@@ -11,6 +11,7 @@ import logo from '../assets/icon.svg';
 
 function Detail() {
   const location = useLocation();
+
 
   let query: string = location.state.query;
   query = query.trim();
@@ -26,10 +27,9 @@ function Detail() {
             aria-label="menu"
             sx={{ mr: 2 }}
           >
-            {/* <MenuIcon /> */}
             <img src={logo} width={32} />
           </IconButton>
-          <Search />
+          <Search search={() => {} }/>
           <Box sx={{ flexGrow: 1 }}/>
           <Button color="inherit">Login</Button>
         </Toolbar>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,15 +2,18 @@ import { Button, Container, Divider, Stack } from "@mui/material";
 import Search from "../components/Search";
 import Typewriter from 'typewriter-effect';
 import './Home.css';
+import { useNavigate } from "react-router-dom";
 
 
 function Home() {
   const strings: string[] = ["我见青山多妩媚，", "料青山见我应如是。", "情与貌，略相似。"];
+  const navgate = useNavigate();
+
 
   return (
     <Container className="container">
       <h1 id="site-title" >古汉语词典</h1>
-      <Search></Search>
+      <Search search={(value) => navgate('/detail', { state: { query: value} } )}/>
       <Typewriter
         options={{
           strings: strings,
@@ -31,3 +34,14 @@ function Home() {
 }
 
 export default Home;
+
+// function search(value: string) {
+//     console.log('输入内容', value);
+//     // todo 增加跳轉
+//     navgate('/detail', {
+//       state: { query: value }
+//     });
+
+//     // 可以使用 navagate(-1)來回退
+//   }
+
