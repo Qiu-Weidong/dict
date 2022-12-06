c630c6c85a5bd80f2f6b06dab26e6b6ed1876cc0
Qiu-Weidong <568385476@qq.com>
2022-12-06 21:04:32 +0800

    ...

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,4 +1,12 @@
 import { useLocation } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Search from '../components/Search';
+import { Button } from '@mui/material';
+
 
 function Detail() {
   const location = useLocation();
@@ -7,7 +15,24 @@ function Detail() {
   query = query.trim();
 
   return (
-      <div></div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" variant='elevation' color='transparent'>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Search />
+          <Box sx={{ flexGrow: 1 }}/>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
