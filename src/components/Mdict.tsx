5f04654aaf459a25ee27511a6eb3c4d98d389754
unknown <568385476@qq.com>
2022-12-09 14:08:30 +0800

    修改了数据库，character拼写错误

--- src/components/Mdict.tsx
+++ src/components/Mdict.tsx
@@ -1,8 +1,8 @@
-import { Card } from "@mui/material";
+import { Avatar, Card, CardHeader, IconButton } from "@mui/material";
 import Chip from "@mui/material/Chip";
-import { Divider } from "@mui/material";
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import eventBus from "../eventbus";
-import { workerData } from "worker_threads";
+import { red } from "@mui/material/colors";
 
 
 export interface DictItem {
@@ -14,16 +14,18 @@ export interface DictItem {
 }
 
 interface Block {
-  header: {
-    charactor: string,
-    level?: string,
-    pronunciation?: string,
-    radical?: string,
-    strokes?: number,
-    struct?: string,
-    variant?: string
-  },
+  header: Header,
   content: Content[][]
+}
+
+interface Header {
+  character: string,
+  level?: string,
+  pronunciation?: string,
+  radical?: string,
+  strokes?: number,
+  struct?: string,
+  variant?: string
 }
 
 interface Content {
@@ -46,25 +48,45 @@ function jump(word: string, character: string) {
 
 
 export function DictItemDisplay(props: { item: DictItem }) {
-
+  const show_title = props.item.blocks ? false : true;
+  const show_prefix: boolean = props.item.prefix ? true : false;
+  const show_related: boolean = props.item.related ? true : false;
   return (
     <Card raised >
-      {/* 首先展示 character */}
-      <h1>{props.item.character}</h1>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" style={{ 'backgroundColor': 'transparent', 'color': 'black', 'fontWeight': 'bold' }} >
+            {props.item.character}
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
 
-      <Divider />
+      {
+        /* 首先展示 character */
+        show_title ? <h3>{props.item.character}</h3> : ''
+      }
+
+
+
       {/* 接下来展示 blocks */}
-      {props.item.blocks?.map( (block, index) => <BlockDisplay block={block} key={index} ></BlockDisplay>)}
+      {props.item.blocks?.map((block, index) => <BlockDisplay block={block} key={index} ></BlockDisplay>)}
 
       {/* 接下来展示词头 */}
       <Chip label="词头" size="small" color="info" />
       {
         props.item.prefix?.map((word, index) => <Chip size="small"
-        variant="outlined" label={word}
-        onClick={() => jump(word, props.item.character) }
-        key={index}
-        color="info"
-        sx={{ m: '1px 2px', }}
+          variant="outlined" label={word}
+          onClick={() => jump(word, props.item.character)}
+          key={index}
+          color="info"
+          sx={{ m: '1px 2px', }}
         />)
       }
 
@@ -73,7 +95,7 @@ export function DictItemDisplay(props: { item: DictItem }) {
       <Chip label="相关词" size="small" color="secondary" />
       {
         props.item.related?.map((word, index) => <Chip
-          onClick={() => jump(word, props.item.character) }
+          onClick={() => jump(word, props.item.character)}
           size="small"
           color='secondary'
           sx={{ m: '1px 2px' }}
@@ -84,17 +106,34 @@ export function DictItemDisplay(props: { item: DictItem }) {
       <br />
 
       {/* 最后展示链接 */}
-      {props.item.link ? <span>见 `<Chip 
+      {props.item.link ? <span>见 `<Chip
         size="small" color="success" variant="outlined" onClick={() => jump(props.item.link as string, props.item.character)}
-        label={props.item.link}/>`</span> : ''}
+        label={props.item.link} />`</span> : ''}
     </Card>
   );
 }
 
-
+function HeaderDisplay(props: { header: Header }) {
+  return (
+    <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" style={{ 'backgroundColor': 'transparent', 'color': 'black', 'fontWeight': 'bold' }} >
+            {props.header.character}
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
+  );
+}
 function BlockDisplay(props: { block: Block }) {
   return (
-    <div>{props.block.header.charactor}</div>
+    <h3>{props.block.header.character}</h3>
   );
 }
 
